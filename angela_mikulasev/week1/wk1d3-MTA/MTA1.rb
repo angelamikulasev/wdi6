71e897995984531e913610a2683d3b6a4e39fa39
angelamikulasev <angela.mikulasev@hotmail.com>
2014-11-13 09:18:01 +1100

    refactored, but need to revisit

--- angela_mikulasev/week1/wk1d3-MTA/MTA1.rb
+++ angela_mikulasev/week1/wk1d3-MTA/MTA1.rb
@@ -1,4 +1,11 @@
 system ("clear")
+
+lines = {
+	'n' => ['Times Square', '34th', '28th_n', '23rd', 'Union Square', '8th_n'],
+	'l' => ['8th_l', '6th', 'Union Square', '3rd', '1st'],
+	'6' => ['Grand Central', '33rd', '28th_6' 'Union Square', 'Astor Place']
+}
+
 def menu
 	puts "1. Line N"
 	puts "2. Line L"
@@ -15,6 +22,10 @@ end
 
 user_choice = read_selection
 
+def number_of_stations
+  (keys.index(destination_station) - keys.index(departure_station)).abs
+end
+
 if 
 	user_choice == '1'
 	puts "What station would you like to depart from? Times Square, 34th, 28th, 23rd, Union Square or 8th"
@@ -29,22 +40,19 @@ else
 end
 
 puts "Where are you departing from?"
-answer = gets.chomp
+departure_station = gets.chomp
 puts "Where is your final destination?"
-answer2 = gets.chomp
+destination_station = gets.chomp
+
+puts "total number of stations: #{ number_of_stations }"
 
 
-line_n = ['Times Square', '34th', '28th_n', '23rd', 'Union Square', '8th_n']
+# number_of_stops = ((lines.index.keys(departure_station)) + (lines.index.keys(destination_station))).abs
+# number_of_stops =  ((line_n.index(answer).abs) + (line_n.index(answer2)).abs)
 
-line_l = ['8th_l', '6th', 'Union Square', '3rd', '1st']
-
-line_6 = ['Grand Central', '33rd', '28th_6' 'Union Square', 'Astor Place']
-
-
-puts line_n.index(answer).abs + line_n.index(answer2).abs
-final_destination = gets.chomp
+# final_destination = gets.chomp
 
 # puts line_l.index(answer).abs + line_l.index(answer2).abs
 # puts line_6.index(answer).abs + line_6.index(answer2).abs
 
-puts "Current number of stop/s inbetween is #{final_destination}."
+# puts "Current number of stop/s inbetween is #{final_destination}."
